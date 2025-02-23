22a8e392c5f68457a702ab481920d99b8f9cb03a
Rhamsez Thevenin <rhamzthev@gmail.com>
2025-02-23 02:04:45 -0500

    added geolocation

--- frontend/components/Main.tsx
+++ frontend/components/Main.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 export default function App() {
 
@@ -11,8 +12,19 @@ export default function App() {
         longitudeDelta: 0.0421,
     })
 
+    const [position, setPosition] = useState<string | null>(null);
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition((pos) => {
+            setPosition(JSON.stringify(pos));
+        });
+    }, [])
+
+    // Geolocation.getCurrentPosition(info => console.log("Hello"));
+
     return (
         <View style={styles.container}>
+            <Text>Position: {position}</Text>
             <MapView initialRegion={region} style={styles.map} />
         </View>
     );
